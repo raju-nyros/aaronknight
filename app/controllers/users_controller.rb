893eb4fff3a603ad349ea47896c70260eb2c6810
Ratnam Raju <ratnamraju.v@nyros.in>
2011-12-09 17:52:10 +0530

    modifications in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ def create
   @agent.user_agent_alias = 'Mac FireFox'
   @user = User.new
   @user.phone =params[:user][:phone]
-  @message =params[:user][:message]
+  @message = "Hi Aaron, I'm Jane. Please call me as soon as you can but not between 3 and 5pm."
   @sms=@agent.get("https://www.my-cool-sms.com/api/send?username=devcomm&password=T2dac8nJ&&number=+91#{params[:user][:phone]}&message=#{@message}")
 
     @user.save
